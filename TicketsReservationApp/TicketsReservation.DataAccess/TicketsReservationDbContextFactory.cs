686b121ba36ac53780ceb257d9e394c2a78f03ff
Zetix7 <billyz7777777@gmail.com>
2024-12-03 15:26:45 +0100

    Create TicketsReservationDbContextFactory class

---
+++ TicketsReservationApp/TicketsReservation.DataAccess/TicketsReservationDbContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace TicketsReservation.DataAccess;
+
+public class TicketsReservationDbContextFactory : IDesignTimeDbContextFactory<TicketsReservationDbContext>
+{
+    public TicketsReservationDbContext CreateDbContext(string[] args)
+    {
+        var optionBuilder = new DbContextOptionsBuilder<TicketsReservationDbContext>();
+        optionBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=TicketsReservationStorage;Integrated Security=True;Trust Server Certificate=True");
+        return new TicketsReservationDbContext(optionBuilder.Options);
+    }
+}
