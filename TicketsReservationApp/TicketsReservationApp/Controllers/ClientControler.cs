320d51f9d92b5b999cd9ba6362bebd3b19ac522d
Zetix7 <billyz7777777@gmail.com>
2024-12-06 14:34:56 +0100

    Create ClientController class, fix bugs in Program.cs file

---
+++ TicketsReservationApp/TicketsReservationApp/Controllers/ClientControler.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TicketsReservation.DataAccess.Entities;
+using TicketsReservation.DataAccess.Repository;
+
+namespace TicketsReservation.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ClientController : ControllerBase
+{
+    private readonly IRepository<Client> _repository;
+
+    public ClientController(IRepository<Client> repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet]
+    [Route("")]
+    public IEnumerable<Client> GetAllClients()
+    {
+        return _repository.GetAll();
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    public Client? GetClientById(int id)
+    {
+        return _repository.GetById(id);
+    }
+}
--- TicketsReservationApp/TicketsReservationApp/Program.cs
+++ TicketsReservationApp/TicketsReservationApp/Program.cs
@@ -6,11 +6,9 @@ using TicketsReservation.DataAccess.Repository;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddSingleton(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddDbContext<TicketsReservationDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("TicketsReservationConnection")));
 
